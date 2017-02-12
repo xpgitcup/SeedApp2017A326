8c750115a55154330bd6b0dae9d466007db8bff5
LiXiaoping <LiXiaoping@Chuyun527-lxp>
2017-02-12 17:03:28 +0800

    增加了监听器

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,3 +1,8 @@
+
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.FileAppender
+
 import grails.util.BuildSettings
 import grails.util.Environment
 import org.springframework.boot.logging.logback.ColorConverter
@@ -10,6 +15,21 @@ conversionRule 'wex', WhitespaceThrowableProxyConverter
 
 // See http://logback.qos.ch/manual/groovy.html for details on configuration
 appender('STDOUT', ConsoleAppender) {
+    encoder(PatternLayoutEncoder) {
+        charset = Charset.forName('UTF-8')
+
+        pattern =
+                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
+                        '%clr(%5p) ' + // Log level
+                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
+                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
+                        '%m%n%wex' // Message
+    }
+}
+
+appender('FILE', FileAppender) {
+    file = "/var/logs/@info.app.name@.log"
+    append = true
     encoder(PatternLayoutEncoder) {
         charset = Charset.forName('UTF-8')
 
@@ -35,5 +55,5 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
     root(ERROR, ['STDOUT', 'FULL_STACKTRACE'])
 }
 else {
-    root(ERROR, ['STDOUT'])
+    root(ERROR, ['FILE'])
 }
